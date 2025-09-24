27ae5e1de3770fa684731704f579790c32ba9f72
otabek <otamurodovotabek990@gmail.com>
2025-09-24 17:26:59 +0500

    while

--- 1-dars.py
+++ 1-dars.py
@@ -1,2 +1,41 @@
-print("hello world")
-print("hello world")
+# Mavzu: while
+
+# juft va toq sonlarni aniqlovchi dastur
+# sonlar = []
+# i = 1
+# while i <= 10:
+#     son = input(f"{i} - sonni kiriting\n>>")
+#     sonlar.append(son)
+#     i += 1
+# i = 0
+# while i < len(sonlar):
+#     if int(sonlar[i]) % 2 == 0:
+#         print(sonlar[i], " - bu juft son")
+#     else:
+#         print(sonlar[i], " - bu toq son")
+#     i += 1
+
+
+
+# from 1 to 10 numbers
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# from 1 to 100 numbers
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+
+
+# faktorialni topish
+son = int(input("son kiriting\n>>"))
+i = 1
+faktorial = 1
+while i <= son:
+    faktorial *= i
+    i += 1
+print("kiritilgan sonning faktoriali:", faktorial)
